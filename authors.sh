8f1492553d4e09d2370d80fcec1e69489d30a9aa
Tomas Dobrovolny <sdobrtomas@gmail.com>
2017-06-11 12:32:08 +0200

    Add AUTHORS.txt
    Add script generating AUTHORS.txt file from commits

---
+++ authors.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# script from http://stackoverflow.com/questions/12133583
+set -e
+AUTHORS_FILE="$(git rev-parse --show-toplevel)/AUTHORS.txt"
+
+# Get a list of authors ordered by number of commits
+# and remove the commit count column
+AUTHORS=$(git --no-pager shortlog -nse --no-merges | cut -f 2- )
+if [ -z "$AUTHORS" ] ; then
+    echo "Authors list was empty"
+    exit 1
+fi
+
+# Display the authors list and write it to the file
+echo "To this project contribute following authors:" | tee "$AUTHORS_FILE"
+echo "$AUTHORS" | tee -a "$AUTHORS_FILE"
+echo -e "\nThank you!" | tee -a "$AUTHORS_FILE"
